32dd6f6327a50ae1390ed2c2080eab153c632fd5
miguelalcosta <miguelcosta217@hotmail.com>
2026-03-28 11:55:41 +0000

    start project

---
+++ DealApp/DealApp/DealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealAppApp.swift
+//  DealApp
+//
+//  Created by Miguel Costa on 28/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
